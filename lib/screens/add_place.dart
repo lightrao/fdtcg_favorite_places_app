03f903618a91ea3a48b6d3fbcf4961cbf6794e95
lightrao <lightrao@hotmail.com>
2024-10-10 07:56:38 +0800

    Adding a "Pick an Image" Input

--- lib/screens/add_place.dart
+++ lib/screens/add_place.dart
@@ -1,4 +1,5 @@
 import 'package:fdtcg_favorite_places_app/providers/user_places.dart';
+import 'package:fdtcg_favorite_places_app/widgets/image_input.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -50,6 +51,10 @@ class _AddPlaceScreenState extends ConsumerState<AddPlaceScreen> {
                 color: Theme.of(context).colorScheme.onSurface,
               ),
             ),
+            const SizedBox(
+              height: 16,
+            ),
+            ImageInput(),
             const SizedBox(
               height: 16,
             ),
