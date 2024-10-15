1874c980092cf290f1e22cd990b0c3ebc9e86443
lightrao <lightrao@hotmail.com>
2024-10-15 19:43:04 +0800

    Using the Picked Location in the Form

--- lib/screens/add_place.dart
+++ lib/screens/add_place.dart
@@ -1,5 +1,6 @@
 import 'dart:io';
 
+import 'package:fdtcg_favorite_places_app/models/place.dart';
 import 'package:fdtcg_favorite_places_app/providers/user_places.dart';
 import 'package:fdtcg_favorite_places_app/widgets/image_input.dart';
 import 'package:fdtcg_favorite_places_app/widgets/location_input.dart';
@@ -18,17 +19,22 @@ class AddPlaceScreen extends ConsumerStatefulWidget {
 class _AddPlaceScreenState extends ConsumerState<AddPlaceScreen> {
   final _titleController = TextEditingController();
   File? _selectedImage;
+  PlaceLocation? _selectedLocation;
 
   void _savePlace() {
     final enteredTitle = _titleController.text;
 
-    if (enteredTitle.isEmpty || _selectedImage == null) {
+    if (enteredTitle.isEmpty ||
+        _selectedImage == null ||
+        _selectedLocation == null) {
       return;
     }
 
-    ref
-        .read(userPlacesProvider.notifier)
-        .addPlace(enteredTitle, _selectedImage!);
+    ref.read(userPlacesProvider.notifier).addPlace(
+          enteredTitle,
+          _selectedImage!,
+          _selectedLocation!,
+        );
     Navigator.of(context).pop();
   }
 
@@ -68,7 +74,11 @@ class _AddPlaceScreenState extends ConsumerState<AddPlaceScreen> {
             const SizedBox(
               height: 16,
             ),
-            LocationInput(),
+            LocationInput(
+              onSelectLocation: (location) {
+                _selectedLocation = location;
+              },
+            ),
             const SizedBox(
               height: 16,
             ),
